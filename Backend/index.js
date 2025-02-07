a9e185964ae88018fc46c29f613f264255dcf398
vaishali86c <vaishali86c@gmail.com>
2025-02-08 00:56:24 +0530

    Folder strcture added and ApiResponse and ApiError added

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,18 @@
-const express = require("express");
+import express from "express";
+import ApiResponse from "./utils/ApiResponse.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 8002
 
 app.get('/', (req, res) => {
-    res.send(" Api working !")
+    // res.send(" Api working !")
+
+    return res.send(new ApiResponse(200, {"name" : "sushant"}))
 })
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
 })
+
