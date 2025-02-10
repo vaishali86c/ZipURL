de086e74c058015bcb08a77f10ca40aee27bd04d
vaishali86c <vaishali86c@gmail.com>
2025-02-11 00:17:19 +0530

    List URL functionality added

--- Backend/controllers/url.controller.js
+++ Backend/controllers/url.controller.js
@@ -10,6 +10,26 @@ const urlSchema = zod.object({
     url: zod.string(),
 })
 
+const listUrl = asyncHandler(async (req, res) => {
+    if(!req._id) {
+        return new ApiError(404, "Invalid Credentials");
+    }
+    const user = await User.findById(req._id);
+    
+    const urlIds = user.urls;
+    let data = [];
+
+    for(let i = 0; i < urlIds.length; i++) {
+        let urlId = urlIds[i];
+        const url = await Url.findById(urlId);
+        data.push({
+            longUrl: url.url,
+            shortId: `${process.env.URL}${url.shortId}`
+        });
+    }
+    return res.status(200).json(new ApiResponse(200, data, "Url Received Successfully"));
+})
+
 const getZipUrl = asyncHandler(async(req, res) => {
 
     if(!urlSchema.safeParse(req.body).success) {
@@ -57,4 +77,4 @@ const handleRedirect = asyncHandler(async(req, res) => {
     return res.redirect(url.url)
 })
 
-export { getZipUrl, handleRedirect }
+export { getZipUrl, handleRedirect, listUrl }
--- Backend/routes/url.route.js
+++ Backend/routes/url.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getZipUrl } from "../controllers/url.controller.js";
+import { getZipUrl, listUrl } from "../controllers/url.controller.js";
 import verifyLogin from "../middlewares/verifyLogin.middleware.js";
 
 
 const router = Router();
 
 router.route("/").get(verifyLogin, getZipUrl)
+router.route("/list").get(verifyLogin, listUrl)
 
 export default router
